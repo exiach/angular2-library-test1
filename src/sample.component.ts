fdcffb11b42e0d3cc7aff6d14d2c35724f325167
Daniel Canqui <daniel.canqui@jalasoft.com>
2016-12-14 18:17:28 -0400

    dd

--- src/sample.component.ts
+++ src/sample.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'sample-component',
   templateUrl: './sample.component.html',
-
+  styleUrls: ['./sample.component.css']
 })
 export class SampleComponent {
 
