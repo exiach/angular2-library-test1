d7c85cf08cd5f8fb0f8cd1c68caa9ca20fbc5b39
Daniel Canqui <daniel.canqui@jalasoft.com>
2016-12-14 18:01:44 -0400

    dd

--- src/sample.component.ts
+++ src/sample.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'sample-component',
-  template: `<h1>Sample component it works</h1>`
+  template: `<h1>Up yours Sergio</h1>`
 })
 export class SampleComponent {
 
